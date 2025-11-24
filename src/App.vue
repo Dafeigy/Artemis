ed94df586ca2245163854d20cde0312ab2554dc9
NUL4i <cybersh1t@126.com>
2025-11-24 17:03:44 +0800

    init: template init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// import { ref } from "vue";
+// import { invoke } from "@tauri-apps/api/core";
+
+// const greetMsg = ref("");
+// const name = ref("");
+
+// async function greet() {
+//   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+//   greetMsg.value = await invoke("greet", { name: name.value });
+// }
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline bg-red-400">
+    Hello world!
+  </h1>
+  <Button>Click me</Button>
+</template>
